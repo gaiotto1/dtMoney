bd376e0b644b5d2e9fb7ce739fffe143ea6d2885
Julio Gaiotto <juliogaiotto@hotmail.com>
2021-05-26 12:01:35 -0300

    Configurando modal de criação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import logo from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt="dtMoney" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
       </Content>
     </Container>
   )
